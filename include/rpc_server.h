e24c9a341b189d5235f4c65417cad8e2d82804f9
qicosmos <qicosmos@163.com>
2019-05-18 09:49:51 +0800

    add connection timeout callback

--- include/rpc_server.h
+++ include/rpc_server.h
@@ -10,86 +10,97 @@
 using boost::asio::ip::tcp;
 
 namespace rest_rpc {
-namespace rpc_service {
-using rpc_conn = std::weak_ptr<connection>;
-class rpc_server : private asio::noncopyable {
- public:
-  rpc_server(short port, size_t size, size_t timeout_seconds = 15, size_t check_seconds = 10)
-      : io_service_pool_(size),
-        acceptor_(io_service_pool_.get_io_service(), tcp::endpoint(tcp::v4(), port)),
-        timeout_seconds_(timeout_seconds),
-        check_seconds_(check_seconds) {
-    do_accept();
-    check_thread_ = std::make_shared<std::thread>([this] { clean(); });
-  }
+	namespace rpc_service {
+		using rpc_conn = std::weak_ptr<connection>;
+		class rpc_server : private asio::noncopyable {
+		public:
+			rpc_server(short port, size_t size, size_t timeout_seconds = 15, size_t check_seconds = 10)
+				: io_service_pool_(size),
+				acceptor_(io_service_pool_.get_io_service(), tcp::endpoint(tcp::v4(), port)),
+				timeout_seconds_(timeout_seconds),
+				check_seconds_(check_seconds) {
+				do_accept();
+				check_thread_ = std::make_shared<std::thread>([this] { clean(); });
+			}
 
-  ~rpc_server() {
-	stop_check_ = true;
-	check_thread_->join();
-    io_service_pool_.stop();
-    thd_->join();
-  }
+			~rpc_server() {
+				stop_check_ = true;
+				check_thread_->join();
+				io_service_pool_.stop();
+				thd_->join();
+			}
 
-  void run() {
-    thd_ = std::make_shared<std::thread>([this] { io_service_pool_.run(); });
-  }
+			void run() {
+				thd_ = std::make_shared<std::thread>([this] { io_service_pool_.run(); });
+			}
 
-  template<ExecMode model = ExecMode::sync, typename Function>
-  void register_handler(std::string const& name, const Function& f) {
-    router::get().register_handler<model>(name, f);
-  }
+			template<ExecMode model = ExecMode::sync, typename Function>
+			void register_handler(std::string const& name, const Function& f) {
+				router::get().register_handler<model>(name, f);
+			}
 
-  template<ExecMode model = ExecMode::sync, typename Function, typename Self>
-  void register_handler(std::string const& name, const Function& f, Self* self) {
-    router::get().register_handler<model>(name, f, self);
-  }
+			template<ExecMode model = ExecMode::sync, typename Function, typename Self>
+			void register_handler(std::string const& name, const Function& f, Self* self) {
+				router::get().register_handler<model>(name, f, self);
+			}
 
- private:
-  void do_accept() {
-    conn_.reset(new connection(io_service_pool_.get_io_service(), timeout_seconds_));
-    acceptor_.async_accept(conn_->socket(), [this](boost::system::error_code ec) {
-      if (ec) {
-        //LOG(INFO) << "acceptor error: " << ec.message();
-      } else {
-        conn_->start();
-        std::unique_lock<std::mutex> lock(mtx_);
-		conn_->set_conn_id(conn_id_);
-        connections_.emplace(conn_id_++, conn_);
-      }
+			void set_conn_timeout_callback(std::function<void(int64_t)> callback) {
+				conn_timeout_callback_ = std::move(callback);
+			}
 
-      do_accept();
-    });
-  }
+		private:
+			void do_accept() {
+				conn_.reset(new connection(io_service_pool_.get_io_service(), timeout_seconds_));
+				acceptor_.async_accept(conn_->socket(), [this](boost::system::error_code ec) {
+					if (ec) {
+						//LOG(INFO) << "acceptor error: " << ec.message();
+					}
+					else {
+						conn_->start();
+						std::unique_lock<std::mutex> lock(mtx_);
+						conn_->set_conn_id(conn_id_);
+						connections_.emplace(conn_id_++, conn_);
+					}
 
-  void clean() {
-    while (!stop_check_) {
-      std::this_thread::sleep_for(std::chrono::seconds(check_seconds_));
+					do_accept();
+				});
+			}
 
-      std::unique_lock<std::mutex> lock(mtx_);
-      for (auto it = connections_.cbegin(); it != connections_.cend();) {
-        if (it->second->has_closed()) {
-          it = connections_.erase(it);
-        } else {
-          ++it;
-        }
-      }
-    }
-  }
+			void clean() {
+				while (!stop_check_) {
+					std::this_thread::sleep_for(std::chrono::seconds(check_seconds_));
 
-  io_service_pool io_service_pool_;
-  tcp::acceptor acceptor_;
-  std::shared_ptr<connection> conn_;
-  std::shared_ptr<std::thread> thd_;
-  std::size_t timeout_seconds_;
+					std::unique_lock<std::mutex> lock(mtx_);
+					for (auto it = connections_.cbegin(); it != connections_.cend();) {
+						if (it->second->has_closed()) {
+							if (conn_timeout_callback_) {
+								conn_timeout_callback_(it->second->conn_id());
+							}
+							it = connections_.erase(it);
+						}
+						else {
+							++it;
+						}
+					}
+				}
+			}
 
-  std::unordered_map<int64_t, std::shared_ptr<connection>> connections_;
-  int64_t conn_id_ = 0;
-  std::mutex mtx_;
-  std::shared_ptr<std::thread> check_thread_;
-  size_t check_seconds_;
-  bool stop_check_ = false;
-};
-}  // namespace rpc_service
+			io_service_pool io_service_pool_;
+			tcp::acceptor acceptor_;
+			std::shared_ptr<connection> conn_;
+			std::shared_ptr<std::thread> thd_;
+			std::size_t timeout_seconds_;
+
+			std::unordered_map<int64_t, std::shared_ptr<connection>> connections_;
+			int64_t conn_id_ = 0;
+			std::mutex mtx_;
+			std::shared_ptr<std::thread> check_thread_;
+			size_t check_seconds_;
+			bool stop_check_ = false;
+
+			std::function<void(int64_t)> conn_timeout_callback_;
+		};
+	}  // namespace rpc_service
 }  // namespace rest_rpc
 
 #endif  // REST_RPC_RPC_SERVER_H_
